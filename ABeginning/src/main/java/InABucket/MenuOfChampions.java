eeec7ac5f47db97112e58336048d7d8b0272f3f0
Mohamed Barry <Mbarry323@gmail.com>
2017-11-30 17:33:24 -0500

    Did up to Practice Programming #2 - Variables

---
+++ ABeginning/src/main/java/InABucket/MenuOfChampions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InABucket;
+
+/**
+ *
+ * @author laptop
+ */
+public class MenuOfChampions {
+    
+    String decoration = " *=*=**=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=";
+    
+    String menuTitle = "                      Welcome To Restaurante Canté";
+    String todaysMenu = "                            Today's Menus is";
+    double pizza = 55;
+    String pizzaCost = "Slice of Pizza.................";
+    double applePie = 5;
+    String applePieCost = "Slice of Pie................";
+    
+//    System.log
+    
+}
