c3319de8f86d9380c1e75ff1b555798d62289d39
Mohamed Barry <Mbarry323@gmail.com>
2017-11-30 21:12:21 -0500

    MBARRYINDIVIDUALWORK-2 up to AllTheTrivia

---
+++ ABeginning/src/main/java/InABucket/AllTheTrivia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InABucket;
+
+/**
+ *
+ * @author laptop
+ */
+public class AllTheTrivia {
+    public static void main(String[] args) {
+        
+        //Working on this. Committing first.
+        
+        
+    }
+}
