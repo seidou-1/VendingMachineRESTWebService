c914e846a85eceb686dd67904e5671c453efa80a
Mohamed Barry <Mbarry323@gmail.com>
2017-12-11 14:07:05 -0500

    MBARRYINDIVIDUALWORK-2 working on HealthyHearts

---
+++ Milestone 1-Java Basics/src/main/java/Assignment_Basic_Programming_Concepts/_3HealthyHearts.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_Basic_Programming_Concepts;
+/**
+ *
+ * @author Kenny
+ */
+ 
+public class _3HealthyHearts {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+} 
