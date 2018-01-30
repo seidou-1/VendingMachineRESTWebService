6d116808fc2754500d10fbde564510a38c404363
Mohamed Barry <Mbarry323@gmail.com>
2018-01-30 01:33:39 -0500

    MBARRYINDIVIDUALWORK-10 configured springDI stage 1 complete

---
+++ mavenprojectVMPM3/src/main/java/_VendingMachine/App.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _VendingMachine;
+
+import UI.UserIO;
+import UI.UserIOImpl;
+import UI.View;
+import VendingMachineController.Controller;
+import VendingMachineDao.Dao;
+import VendingMachineDao.DaoFileImpl;
+import VendingMachineDao.PersistenceException;
+import VendingMachineDto.Change;
+import VendingMachineServiceLayer.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author laptop
+ */
+public class App {
+
+    public static void main(String[] args) throws PersistenceException, InventoryAvailabilityException, InsufficientFundsException, DataValidationException {
+
+//        //Using this particular implementation of the Dao 
+//       Dao hashMap = new DaoFileImpl();
+//       
+//       //Using this particular implementation of the userIO
+//       UserIO userIO = new UserIOImpl();
+//       
+//       //Passing this particular userIO impl to the view
+//       View theView = new View(userIO);
+//       
+//       //Adding the service layer
+//       Service theService = new ServiceImpl(hashMap);
+//       
+//       Change theChange = new Change();
+//       
+//       //Also instantiating the controller
+//       Controller theController = new Controller (theService, theView, theChange);
+//       
+//       theController.run();
+        ApplicationContext ctx
+                = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Controller controller
+                = ctx.getBean("controller", Controller.class);
+        controller.run();
+
+    }
+
+}
